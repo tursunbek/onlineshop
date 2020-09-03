d15c8f471ce8002cb7103fbc6acfccbd69073ed8
tursunbek <tiku_kg@list.ru>
2020-09-03 13:08:25 +0600

    added and views file with three functions cart_add/remove/detail

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 PRODUCT_QUANTITY_SHOICES = [(i, str(i)) for i in range(1,21)]
 
-class CardAddProductForm(forms.Form):
+class CartAddProductForm(forms.Form):
     quantity=forms.TypedChoiceField(shoices=PRODUCT_QUANTITY_SHOICES, coerce=int)
     update = forms.BooleanField(request=False, initial=False, widget=forms.hiddenInput)
 
