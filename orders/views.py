6beb4f8c6bc2846dd757916b0e7226a5d91a32ed
tursunbek <tiku_kg@list.ru>
2020-09-05 12:29:09 +0600

    added function order_create and added file urls in directory orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
 # Create your views here.
+from .forms import OrderCreateForm
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.object.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['product'],
+                    quantity=item['quantity']
+                )
+            cart.clear()
+            return render(request, 'created.html', {'order':order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'created.html', {'cart':cart, 'form':form})
